0b52d83ccb1814d7a673230d894e644ec8a137c4
Emersonhd <56261106+Emersonhd@users.noreply.github.com>
2020-03-01 17:23:11 +0000

    Doxy-Comments
    Added Doxy-Comments to model.

--- model.h
+++ model.h
@@ -13,28 +13,79 @@ class Image;
 class Model
 {
     Control *control;
+
+
     std::vector<std::string> imageNames;
+    //!A public function
+    /*!
+     * Maps the images.
+     */
     std::map<std::string, Image * > images;
+
     std::vector<std::string> classifierNames;
+    //!A public function
+    /*!
+     * Gets and returns the image dates in ascending order into imageNamesDatesAsc.
+     */
     std::vector<std::string> imageNameDatesAsc;
+
     std::vector<std::string> imageNameDatesDec;
 public:
     Model(Control *cont = nullptr);
 
     std::vector<std::string> getImageNames() {return imageNames;}
+    //!A public function
+    /*!
+     * Gets and returns the classifier names into classifierNames.
+     */
     std::vector<std::string> getClassifierNames() {return classifierNames;}
+    //!A public function
+    /*!
+     * Gets and returns the image dates in decending order into imageNamesDatesDec.
+     */
     std::vector<std::string> getImageNameDatesAsc() {return imageNameDatesAsc;}
+    //!A public function
+    /*!
+     * Gets and returns the image names into imageNames.
+     */
     std::vector<std::string> getImageNameDatesDec() {return imageNameDatesDec;}
 
+    //!A list of all pens
+    /*!
+    The QMap is an access method used to retrieve data it stores a list of all pen objects; line, shape, .appoint to be used when drawing shapes
+    */
     QMap<std::string,QPen> requestPens();
+
     QGraphicsPixmapItem * requestImageItem(std::string imageName);
+
     std::string requestMode();
     std::string requestMode2();
     std::string requestMode3();
+    //!A public function
+    /*!
+     * Makes a request to attempt connecting the last drawn points, if successful it will connect them.
+     * \param imageName
+     */
     void requestConnectLastDrawnPoints(std::string imageName);
+    //!A public function
+    /*!
+     * Makes a request to attempt to add a drawn shape,if successful it will draw the shape.
+     * \param imageName
+     */
     void requestAddDrawnShape(std::string imageName);
-
+    //!A public function
+    /*!
+     * Loads the dataset into the program and gui, also enables the ability to sort in ascending or decending order.
+     * \param folderPath
+     *
+     */
     std::string loadDataset(std::string folderPath);
+    //!A public function
+    /*!
+     * Loads the classifiers into the program.
+     * \param filePath
+     *
+     */
     std::string loadClassifers(std::string filePath);
     void loadImage(QString imagePath, const QString imageName);
     QPixmap loadImage(const QString imagePath);
